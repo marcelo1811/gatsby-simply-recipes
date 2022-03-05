47c6c3d6b45aa03144540a21cd1710d19f060f66
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-05 18:28:45 -0300

    feat: add layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React from "react"
+import Layout from "../components/Layout"
 import Navbar from "../components/Navbar"
 
 const About = () => {
   return (
-    <div>
-      <Navbar />
+    <Layout>
       <h2>hello from About page</h2>
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Layout from "../components/Layout"
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <h1>Hello asdfsworlds!</h1>
-    </div>
+    <Layout>
+      <h1>Hello from home!</h1>
+    </Layout>
   )
 }
