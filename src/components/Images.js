d3aa9b02c6ef3df60f00354c57fe60a06305fd85
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-06 11:25:51 -0300

    feat: add image examples

--- src/components/Images.js
+++ src/components/Images.js
@@ -10,11 +10,33 @@ const Images = () => {
         <StaticImage
           src="../assets/images/recipes/recipe-1.jpeg"
           alt="food"
-          height={400}
           placeholder="tracedSVG"
           layout="constrained"
           className="example-img"
           as="section"
+        />
+      </article>
+      <article>
+        <h4>fixed</h4>
+        <StaticImage
+          src="../assets/images/recipes/recipe-1.jpeg"
+          alt="food"
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="example-img"
+          as="div"
+        />
+      </article>
+      <article>
+        <h4>full width</h4>
+        <StaticImage
+          src="../assets/images/recipes/recipe-1.jpeg"
+          alt="food"
+          placeholder="dominantcolor"
+          layout="fullWidth"
+          className="example-img"
+          as="article"
         />
       </article>
     </Wrapper>
@@ -24,11 +46,17 @@ const Images = () => {
 const Wrapper = styled.section`
   width: 70vw;
   margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
   article {
     border: 2px solid red;
   }
   .example-img {
     border-radius: 1rem;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(3, 1fr);
   }
 `
 
