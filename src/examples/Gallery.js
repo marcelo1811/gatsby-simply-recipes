5240cdf1c5244839417f8520877ef0cbdd175588
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-09 21:37:57 -0300

    feat: add gatsby images on gallery

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -1,14 +1,31 @@
 import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import React from "react"
+import styled from "styled-components"
 
 const Gallery = () => {
   const data = useStaticQuery(query)
-  return <div>Gallery</div>
+  const nodes = data.allFile.nodes
+  return (
+    <Wrapper>
+      {nodes.map((image, index) => {
+        const { name } = image
+        return (
+          <article key={index}>
+            <GatsbyImage image={image.childImageSharp.gatsbyImageData} />
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section``
 
 const query = graphql`
   {
-    allFile(filter: { extension: { ne: "svg" } }) {
+    allFile(filter: { extension: { nin: ["svg", "ico"] } }) {
       nodes {
         name
         childImageSharp {
@@ -16,6 +33,7 @@ const query = graphql`
             layout: FIXED
             placeholder: BLURRED
             transformOptions: { grayscale: true }
+            width: 200
           )
         }
       }
