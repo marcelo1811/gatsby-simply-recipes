669706125b3bb5daa23d6b00d1ca21d40d542608
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-09 21:28:34 -0300

    feat: add Gallery query

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -1,7 +1,26 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 
 const Gallery = () => {
+  const data = useStaticQuery(query)
   return <div>Gallery</div>
 }
+
+const query = graphql`
+  {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: BLURRED
+            transformOptions: { grayscale: true }
+          )
+        }
+      }
+    }
+  }
+`
 
 export default Gallery
