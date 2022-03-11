95326447a517a93f50fea6a5fe876f1a7bbb6403
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-11 07:41:42 -0300

    feat: add gallery with styles

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -3,26 +3,6 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import React from "react"
 import styled from "styled-components"
 
-const Gallery = () => {
-  const data = useStaticQuery(query)
-  const nodes = data.allFile.nodes
-  return (
-    <Wrapper>
-      {nodes.map((image, index) => {
-        const { name } = image
-        return (
-          <article key={index}>
-            <GatsbyImage image={image.childImageSharp.gatsbyImageData} />
-            <p>{name}</p>
-          </article>
-        )
-      })}
-    </Wrapper>
-  )
-}
-
-const Wrapper = styled.section``
-
 const query = graphql`
   {
     allFile(filter: { extension: { nin: ["svg", "ico"] } }) {
@@ -32,8 +12,9 @@ const query = graphql`
           gatsbyImageData(
             layout: FIXED
             placeholder: BLURRED
-            transformOptions: { grayscale: true }
+            transformOptions: { grayscale: false }
             width: 200
+            height: 200
           )
         }
       }
@@ -41,4 +22,36 @@ const query = graphql`
   }
 `
 
+const Gallery = () => {
+  const data = useStaticQuery(query)
+  const nodes = data.allFile.nodes
+  return (
+    <Wrapper>
+      {nodes.map((image, index) => {
+        const { name } = image
+        return (
+          <article key={index} className="item">
+            <GatsbyImage
+              image={image.childImageSharp.gatsbyImageData}
+              alt={name}
+              className="gallery-img"
+            />
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    margin-right: 1rem;
+  }
+  .gallery-img {
+    border-radius: 1rem;
+  }
+`
 export default Gallery
