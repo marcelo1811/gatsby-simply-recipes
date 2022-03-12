7db7a57c9b48b9251f74ed735b63d564eeee82f6
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-12 15:45:04 -0300

    fix(recipePage): styles

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -23,39 +23,39 @@ const RecipeTemplate = ({ data }) => {
         <div className="recipe-page">
           <section className="recipe-hero">
             <GatsbyImage image={pathToImage} alt={title} className='about-img' />
-          </section>
-          <section className="recipe-content">
-            <h2>{title}</h2>
-            <p>{description}</p>
-            {/* icons */}
-            <div className="recipe-icons">
-              <article>
-                <BsClock />
-                <h5>prep time</h5>
-                <p>{prepTime} min.</p>
-              </article>
-              <article>
-                <BsClockHistory />
-                <h5>cook time</h5>
-                <p>{cookTime} min.</p>
-              </article>
-              <article>
-                <BsPeople />
-                <h5>serving</h5>
-                <p>{servings}</p>
-              </article>
-            </div>
-            {/* tags */}
-            <p className="recipe-tags">
-              Tags:
-              {tags.map((tag, index) => {
-                return (
-                  <Link to={`/${tag}`} key={index}>
-                    {tag}
-                  </Link>
-                )
-              })}
-            </p>
+            <section className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>serving</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className="recipe-tags">
+                Tags:
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </section>
           </section>
           {/* rest of the content */}
           <section className="recipe-content">
@@ -74,6 +74,16 @@ const RecipeTemplate = ({ data }) => {
             <article className="second-column">
               <div>
                 <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return (
+                    <div className='single-tool' key={index}>
+                      {item}
+                    </div>
+                  )
+                })}
+              </div>
+              <div>
+                <h4>tools</h4>
                 {tools.map((item, index) => {
                   return (
                     <div className='single-tool' key={index}>
