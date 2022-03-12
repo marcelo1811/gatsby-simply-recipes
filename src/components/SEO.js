41d5f075036eee19ff60607fccbdb5bb766a57aa
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-12 16:42:23 -0300

    feat: add SEO

---
+++ src/components/SEO.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+const SEO = () => {
+  return (
+    <Helmet title='home'></Helmet>
+  )
+}
+
+export default SEO
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import AllRecipes from "../components/AllRecipes"
 import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 
 const Home = () => {
   return (
     <Layout>
+      <SEO />
       <main className="page">
         <header className="hero">
           <StaticImage
