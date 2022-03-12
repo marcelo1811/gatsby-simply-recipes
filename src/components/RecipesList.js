f380799c8592d9611b0d64f83dd1564de9815c52
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-12 11:03:20 -0300

    fix(RecipesLst): fix link with slugify

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,14 +1,17 @@
 import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
+import slugify from "slugify"
 
 const RecipesList = ({ recipes = [] }) => {
   return (
     <div className="recipes-list">
       {recipes.map(({ id, title, image, prepTime, cookTime }) => {
         const pathToImage = getImage(image)
+        const slug = slugify(title, { lower: true });
+
         return (
-          <Link key={id} to={`/${title}`} className="recipe">
+          <Link key={id} to={`/${slug}`} className="recipe">
             <GatsbyImage
               className="recipe-img"
               image={pathToImage}
