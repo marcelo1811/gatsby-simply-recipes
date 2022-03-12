30133dd72ca1f4f43c85293e914551afd43661ed
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-12 16:17:17 -0300

    feat: add tag-template

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,9 +1,39 @@
+import { graphql } from 'gatsby'
 import React from 'react'
+import Layout from '../components/Layout'
+import RecipesList from '../components/RecipesList'
 
-const TagTemplate = () => {
+const TagTemplate = ({ data, pageContext }) => {
+  const recipes = data.allContentfulRecipe.nodes
   return (
-    <div>TagTemplate</div>
+    <Layout>
+      <main className="page">
+        <h2>{pageContext.tag}</h2>
+        <div className="tag-recipes">
+          <RecipesList recipes={recipes} />
+        </div>
+      </main>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query GetRecipeByTag($tag: String) {
+    allContentfulRecipe(
+      sort: {fields: title, order: ASC}
+      filter: {content: {tags: {eq: $tag}}}
+    ) {
+      nodes {
+        title
+        id
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+      }
+    }
+  }
+`
 
 export default TagTemplate
