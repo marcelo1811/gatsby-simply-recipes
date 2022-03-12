bc7c02ff8a8027f64da7858e1a8b23d7c8fe0b54
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-03-12 15:26:17 -0300

    feat(Recipe): add page query

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import { graphql } from 'gatsby';
+import { getImage } from 'gatsby-plugin-image';
+import React from 'react';
+import Layout from '../components/Layout';
 
-const RecipeTemplate = (props) => {
+const RecipeTemplate = ({ data }) => {
+  const {
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image
+  } = data.contentfulRecipe
+  const pathToImage = getImage(image)
+  const { tags, instructions, ingredients, tools } = content
+
   return (
-    <div>RecipeTemplate</div>
+    <Layout>
+      <main className='page'>
+        <div className="recipe-page">
+          <h2>title</h2>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: {eq: $title}) {
+      title
+      cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
 export default RecipeTemplate
